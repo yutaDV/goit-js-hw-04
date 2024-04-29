ebc1a4faf3d12218032625c1fa204e687ee8bb27
Yuliia <74568169+yutaDV@users.noreply.github.com>
2024-04-29 23:09:38 +0300

    start hw4

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,27 @@
-function slugify(title) {
-    const words = title.split(' ');
-    const slug = words.map(word => word.toLowerCase()).join('-');
-    return slug;
-};
+function isEnoughCapacity(products, containerSize) {
+    let totalQuantity = 0;
+
+    for (let product in products) {
+        totalQuantity += products[product];
+    }
+
+    return totalQuantity <= containerSize;
+}
 
 
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
